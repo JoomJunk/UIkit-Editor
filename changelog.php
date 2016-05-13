511af32f22637feb96cd398f3e188b160f0ff0f9
C-Lodder <lodder15@hotmail.co.uk>
2016-05-13 15:44:29 +0100

    Added changelog + v1.0.0 release

---
+++ changelog.php
@@ -0,0 +1,18 @@
+<?php
+// No direct access
+die('Restricted access');
+?>
+
+Changelog
+------------
+
+* -> Security Fix
+# -> Bug Fix
+$ -> Language fix or change
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+Version 1.0.0
++ Initial release
